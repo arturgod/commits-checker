5d0f80d6018c91825c16526f3ef17e86be94cb1f
arturasg <a.p.godlewski@gmail.com>
2018-12-02 16:18:31 +0100

    index.js fixed

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const git = require('simple-git');
+const gitP = require('simple-git/promise');
 const fs = require('fs');
 const util = require('util')
 const path = './repo-tmp';
 const repoUrl = 'https://github.com/arturgod/monorepo.git';
 const fs_writeFile = util.promisify(fs.writeFile)
 const fs_readFile = util.promisify(fs.readFile)
+
 // works
 // git()
 //   .silent(false)
@@ -41,93 +43,47 @@ const fs_readFile = util.promisify(fs.readFile)
 //   )
 // );
 
-// works
-// let logz;
-// let branch;
-// git(path)
-//   .status(
-//     (err, status) =>{
-//       branch = status.current;
-//     // console.log(status.current);
-//     })
-//     .log(
-//       (err, log) => {
-//         // console.log('logzzz', logz);
-//       logz = log;
-//       const logString = JSON.stringify(logz);
-//       fs.writeFile(`logz-${branch}.json`, logString, function(err) {
-//         if(err) {
-//             return console.log(err);
-//         }
-//         console.log("The file was saved!");
-//       });
-//       }  
-//     )
-//     .then (
-//       ()=> {
-//         // console.log(logz)
-//         console.log('logstttt')
-        
-//       }
-//     )
 
 let logz;
 let branch;
+const readFilePromise = function(){
+  return new Promise(
+    (resolve, reject) => {
+      fs.readFile(`logz-master.json`, {encoding: 'utf-8'}, function(err,data){
+        if(err) {reject(err)}
+        else {
+          console.log('reading: ', data);
+          resolve(data)
+        }
+      }  
+    )}
+  )
+};
+
+const writeFilePromise = function(file, content){
+  return new Promise(
+    (resolve, reject) => {
+      fs.writeFile( file, content, function(err,data){
+        if(err) {reject(err)}
+        else {
+          console.log('writing...');
+          resolve(data)
+        }
+      }  
+    )}
+  )
+}
+
 git(path)
   .status(
     (err, status) =>{
       branch = status.current;
-    // console.log(status.current);
   })
   .log(
     (err, log) => {
-    // console.log('logzzz', logz);
-    logz = log;
-    const logString = JSON.stringify(logz);
-    const b = fs_readFile(`logz-master.json`, {encoding: 'utf-8'}, function(err,data){
-      if (err) {
-        console.log(err);       
-      } else {
-        console.log('received data1: ' + data);
-        const a=  fs_writeFile(`logz-${branch}.json`, logString, function(err) {
-          if(err) {
-              console.log(err);
-          } 
-        })
-        a.then(
-          ()=>{
-            const c = fs_readFile(`logz-master.json`, {encoding: 'utf-8'}, function(err,data){
-              if (!err) {
-                  console.log('received data 2: ' + data);              
-              } else {
-                  console.log(err);
-              }
-            });
-            c.then(
-              (suc)=>{
-                console.log('b suc', suc)
-              },
-              (error)=>{
-                console.log('b error', error)
-              }
-            )
-          }
-        )
-      }
-    });
-    console.log(b)
-    b.then(  
-      console.log('b then')
-    )//b.then
-    .catch(
-      (err)=>console.log('errrrr', err)
-    )
-    console.log(b)
-
+      logz = JSON.stringify(log);
     }
   )
   .then(
-    ()=>{
-      console.log('then po .log')
-    }
+    ()=>writeFilePromise(`logz-${branch}.json`, logz)
   );
